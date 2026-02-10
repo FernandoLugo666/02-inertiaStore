c8aa4524c37a440a7565769595aad6765c9623bb
Fernando Lugo <fernandolugo2-02@hotmail.com>
2026-02-10 17:37:40 -0600

    Controller completo de category,

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
 class CategoryController extends Controller
@@ -22,15 +23,18 @@ class CategoryController extends Controller
         ]);
     }
 
-
-    public function listCategory()
+    public function listCategory(Request $request)
     {
         $data = Category::get();
 
+        $dataPaginada = Category::paginate(2);
+        $dataperPage = Category::paginate($request->input('per_page', 5));
         return Inertia::render(
             "Dashboard/Category/ListCategory",
             [
-                "data" => $data
+                "data" => $data,
+                "dataPaginada" => $dataPaginada,
+                "dataperPage" => $dataperPage
             ]
         );
     }
@@ -48,5 +52,17 @@ class CategoryController extends Controller
         }
 
         return Inertia::render('Dashboard/Category/UpdateCategory', ["data" => $data]);
+    }
+
+    public function deleteCategory($id)
+    {
+        $data = Category::findOrFail($id);
+        try {
+            $data->delete();
+
+            return back()->with('success', 'Registro eliminado correctamente');
+        } catch (\Throwable $th) {
+            Log::debug("Error deleteCategory " . $th->getMessage());
+        }
     }
 }
