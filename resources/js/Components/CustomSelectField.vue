1bc32f162506d3e13cdd67faf4d3db3e00b2ec42
Fernando Lugo <fernandolugo2-02@hotmail.com>
2026-03-25 12:57:03 -0600

    Componente select field

---
+++ resources/js/Components/CustomSelectField.vue
@@ -0,0 +1,47 @@
+<template>
+  <FloatLabel>
+    <Select
+      :modelValue="modelValue"
+      @update:modelValue="updateValue"
+      :options="options"
+      :optionLabel="optionLabel"
+      :placeholder="placeholder"
+      :class="['w-full', { 'font-bold': disabled }]"
+    />
+    <label>{{ label }} <span v-if="required" class="text-red-500">*</span></label>
+  </FloatLabel>
+</template>
+<script setup>
+import { FloatLabel, Select } from "primevue";
+
+const props = defineProps({
+  modelValue: {
+    type: [Object, String, Number, null],
+    default: null,
+  },
+
+  options: {
+    type: Array,
+    required: true,
+  },
+  optionLabel: {
+    type: String,
+    default: "name",
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  label: String,
+  required: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const updateValue = (value) => {
+  emit("update:modelValue", value);
+};
+</script>
